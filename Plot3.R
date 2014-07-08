d75fbd56f7e670eee2302ec0615a3151180191a6
Leonardo Alvarado <joleonar@gmail.com>
2014-07-08 11:18:04 -0430

    Se modifico la posicion de legend

--- Plot3.R
+++ Plot3.R
@@ -9,10 +9,8 @@ plot(X,SM1,type="n",xlab="",ylab="Energy Sub metering",cex.lab=0.8)
 lines(X,SM1)
 lines(X,SM2,col="red")
 lines(X,SM3,col="blue")
-tx="2007-02-02 08:00:00.00"
-tx2 <- strptime(tx,format="%Y-%m-%d %H:%M:%S")
-legend(tx2,40,c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),
-       col=c("black","red","blue"),cex=0.8)
+legend("topright" ,40,c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),
+       col=c("black","red","blue"),cex=0.9)
 
 dev.copy(png,file="Plot3.png",width=480,height=480)
 dev.off()
